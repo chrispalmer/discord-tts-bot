e66ffd1191086f90726c0bd58ab02d1a442b4b83
Chris Palmer <cpalmer@6river.com>
2020-05-05 15:10:05 -0400

    fix preinstal script

--- src/preinstall.js
+++ src/preinstall.js
@@ -1,6 +1,10 @@
-const fs=require(‘fs’);
+const fs = require('fs');
+const { Logger } = require('logger');
 
+const logger = new Logger();
 
 let buff = new Buffer(process.env.GOOGLE_CREDENTIALS_BASE64, 'base64');
 
-fs.writeFile(‘../config/google-credentials-heroku.json’, buff.toString('ascii'), (err) => {});
+fs.writeFile('../config/google-credentials-heroku.json', buff.toString('ascii'), (err) => {
+	logger.error(`Error writing credentials file: ${err}`);
+});
