97c0a2c665bbed353e7e622ad29f264b2b59bc3d
Chris Palmer <cpalmer@6river.com>
2020-05-05 15:22:52 -0400

    use raw credentials in environment variable

--- src/preinstall.js
+++ src/preinstall.js
@@ -3,8 +3,8 @@ const { Logger } = require('logger');
 
 const logger = new Logger();
 
-let buff = new Buffer(process.env.GOOGLE_CREDENTIALS, 'base64');
+// let buff = new Buffer(process.env.GOOGLE_CREDENTIALS, 'base64');
 
-fs.writeFile('../config/google-credentials-heroku.json', buff.toString('ascii'), (err) => {
+fs.writeFile('../config/google-credentials-heroku.json', process.env.GOOGLE_CREDENTIALS_RAW, (err) => {
 	logger.error(`Error writing credentials file: ${err}`);
 });
